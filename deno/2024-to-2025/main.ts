e46350bba1eda7431de03541da17a12f855b6f60
narumincho <16481886+narumincho@users.noreply.github.com>
2024-12-17 08:46:57 +0900

    use mask

--- deno/2024-to-2025/main.ts
+++ deno/2024-to-2025/main.ts
@@ -1,10 +1,14 @@
 import { decode, Image } from "https://deno.land/x/imagescript@1.3.0/mod.ts";
 import {
+  ColorId,
   colorIdFromImageMagicColor,
   colorIdToImageMagicColor,
 } from "./color.ts";
 
 const size = 16 * 8;
+
+const startY = -60;
+// const startY = 64;
 
 const readImageFromFile = async (url: URL): Promise<Image> => {
   const image = await decode(await (await fetch(url)).arrayBuffer());
@@ -100,9 +104,142 @@ const oppositeDirection = (direction: Direction): Direction => {
   }
 };
 
+type CellType = {
+  readonly type: "singleFallen";
+  readonly colorId: ColorId;
+} | {
+  readonly type: "singleNormal";
+  readonly colorId: ColorId;
+  readonly height: number;
+  readonly direction: Direction;
+} | {
+  readonly type: "singleLight";
+  readonly colorId: ColorId;
+} | {
+  readonly type: "dualFallen";
+  readonly lowerColorId: ColorId;
+  readonly upperColorId: ColorId;
+  readonly height: number;
+  readonly direction: Direction;
+} | {
+  readonly type: "dualNormal";
+  readonly lowerColorId: ColorId;
+  readonly upperColorId: ColorId;
+  readonly height: number;
+  readonly direction: Direction;
+} | {
+  readonly type: "dualLight";
+  readonly lowerColorId: ColorId;
+  readonly upperColorId: ColorId;
+  readonly height: number;
+  readonly direction: Direction;
+};
+
+const setBlock = (position: Position, y: number, block: string): string => {
+  return `setblock ~${position.x} ${y} ~${position.z} ${block}`;
+};
+
+const maskColorIdToCellTypeTag = (colorId: ColorId): CellType["type"] => {
+  switch (colorId) {
+    case "orange":
+      return "singleFallen";
+    case "red":
+      return "singleNormal";
+    case "yellow":
+      return "singleLight";
+    case "green":
+      return "dualFallen";
+    case "black":
+      return "dualNormal";
+    case "gray":
+      return "dualLight";
+  }
+  throw new Error(`Unexpected colorId for mask: ${colorId}`);
+};
+
+const createCellCommand = (
+  position: Position,
+  cellType: CellType,
+) => {
+  switch (cellType.type) {
+    case "singleFallen":
+      return setBlock(position, startY, `${cellType.colorId}_wool`);
+    case "singleNormal":
+      return `${
+        setBlock(
+          position,
+          cellType.height - 1,
+          `wall_torch[facing=${oppositeDirection(cellType.direction)}]`,
+        )
+      }
+${setBlock(position, cellType.height, `${cellType.colorId}_concrete_powder`)}`;
+    case "singleLight":
+      return `${setBlock(position, startY - 1, "verdant_froglight")}
+${setBlock(position, startY, `${cellType.colorId}_stained_glass`)}`;
+    case "dualFallen":
+      return `${
+        setBlock(
+          position,
+          startY,
+          `${cellType.lowerColorId}_wool`,
+        )
+      }
+${
+        setBlock(
+          position,
+          cellType.height - 1,
+          `wall_torch[facing=${oppositeDirection(cellType.direction)}]`,
+        )
+      }
+${setBlock(position, cellType.height, `${cellType.upperColorId}_carpet`)}`;
+    case "dualNormal":
+      return `${
+        setBlock(
+          position,
+          cellType.height - 1,
+          `wall_torch[facing=${oppositeDirection(cellType.direction)}]`,
+        )
+      }
+${
+        setBlock(
+          position,
+          cellType.height,
+          `${cellType.lowerColorId}_concrete_powder`,
+        )
+      }
+${setBlock(position, cellType.height + 1, `${cellType.upperColorId}_carpet`)}`;
+    case "dualLight":
+      return `${setBlock(position, startY - 1, "verdant_froglight")}
+${
+        setBlock(
+          position,
+          startY,
+          `${cellType.lowerColorId}_stained_glass`,
+        )
+      }
+${
+        setBlock(
+          position,
+          cellType.height - 1,
+          `wall_torch[facing=${oppositeDirection(cellType.direction)}]`,
+        )
+      }
+${setBlock(position, cellType.height, `${cellType.upperColorId}_carpet`)}`;
+  }
+};
+
+const getImageColorId = (image: Image, position: Position): ColorId => {
+  return colorIdFromImageMagicColor(
+    image.getPixelAt(1 + position.x, 1 + position.z),
+  );
+};
+
 const createCommands = (
-  lower: Image,
-  upper: Image,
+  { mask, lower, upper }: {
+    readonly mask: Image;
+    readonly lower: Image;
+    readonly upper: Image;
+  },
 ): string => {
   const commands: string[] = [];
   /** コンクリートパウダーのY座標を格納するテーブル */
@@ -115,75 +252,56 @@ const createCommands = (
     { length: size },
   ).flatMap((_, x) => Array.from({ length: size }, (_, z) => ({ x, z })));
 
-  const setHeight = (
+  const setCell = (
     position: Position,
-    height: number,
-    direction: Direction,
+    height: number | undefined,
   ): void => {
-    const lowerColorId = colorIdFromImageMagicColor(
-      lower.getPixelAt(1 + position.x, 1 + position.z),
-    );
-    const upperColorId = colorIdFromImageMagicColor(
-      upper.getPixelAt(1 + position.x, 1 + position.z),
-    );
-    const light = (position.x % 8) === 0 && (position.z % 8) === 0;
-    if (!light && lowerColorId !== upperColorId) {
+    if (typeof height === "number") {
       heightTable[position.x]![position.z] = height;
     }
     blankPositions.splice(
       blankPositions.findIndex((p) => p.x === position.x && p.z === position.z),
       1,
     );
-
-    if (light) {
-      commands.push(
-        `setblock ~${position.x} ${startY} ~${position.z} ${lowerColorId}_stained_glass`,
-      );
-      commands.push(
-        `setblock ~${position.x} ${
-          startY - 1
-        } ~${position.z} verdant_froglight`,
-      );
-      if (lowerColorId === upperColorId) {
-        return;
-      }
-      commands.push(
-        `setblock ~${position.x} ${
-          height - 1
-        } ~${position.z} wall_torch[facing=${oppositeDirection(direction)}]`,
-      );
-      commands.push(
-        `setblock ~${position.x} ${height} ~${position.z} ${upperColorId}_carpet`,
-      );
-    } else {
-      commands.push(
-        `setblock ~${position.x} ${
-          height - 1
-        } ~${position.z} wall_torch[facing=${oppositeDirection(direction)}]`,
-      );
-      commands.push(
-        `setblock ~${position.x} ${height} ~${position.z} ${lowerColorId}_concrete_powder`,
-      );
-
-      if (lowerColorId === upperColorId) {
-        return;
-      }
-      commands.push(
-        `setblock ~${position.x} ${
-          height + 1
-        } ~${position.z} ${`${upperColorId}_carpet`}`,
-      );
-    }
   };
 
   const startPosition: Position = { x: 63, z: 118 };
-  const startY = -60;
-  // const startY = 64;
-  commands.push(
-    `setblock ~${startPosition.x} ${startY} ~${startPosition.z + 1} stone`,
-  );
-  setHeight(startPosition, startY + 1, "south");
 
+  commands.push(
+    `setblock ~${startPosition.x} ${startY + 1} ~${startPosition.z + 1} stone`,
+  );
+  commands.push(
+    createCellCommand(startPosition, {
+      type: "dualNormal",
+      lowerColorId: getImageColorId(lower, startPosition),
+      upperColorId: getImageColorId(upper, startPosition),
+      direction: "north",
+      height: startY + 2,
+    }),
+  );
+  setCell(startPosition, startY + 1);
+
+  for (let x = 0; x < size; x++) {
+    for (let z = 0; z < size; z++) {
+      const cellTypeTag = maskColorIdToCellTypeTag(
+        getImageColorId(mask, { x, z }),
+      );
+      if (cellTypeTag === "singleFallen" || cellTypeTag === "singleLight") {
+        commands.push(
+          createCellCommand(
+            { x, z },
+            {
+              type: cellTypeTag,
+              colorId: getImageColorId(lower, { x, z }),
+            },
+          ),
+        );
+        setCell({ x, z }, undefined);
+      }
+    }
+  }
+
+  let skipCount = 0;
   while (true) {
     if (blankPositions.length === 0) {
       return commands.join("\n");
@@ -193,11 +311,88 @@ const createCommands = (
     const direction = randomDirection();
     const basePosition = moveDirection(targetPosition, direction);
     const baseHeight = heightTable[basePosition.x]?.[basePosition.z];
-    if (baseHeight === undefined || baseHeight > 318) {
+    if (baseHeight === undefined) {
+      skipCount++;
+      if (skipCount > 100000) {
+        console.log("Too many skip");
+        return commands.join("\n");
+      }
       console.log("skip", blankPositions.length);
       continue;
     }
-    setHeight(targetPosition, baseHeight + 1, direction);
+    if (baseHeight > 318) {
+      console.log("skip too height", blankPositions.length);
+      continue;
+    }
+    skipCount = 0;
+    const cellTypeTag = maskColorIdToCellTypeTag(
+      getImageColorId(mask, targetPosition),
+    );
+    switch (cellTypeTag) {
+      case "singleFallen":
+        throw new Error("Unexpected singleFallen");
+      case "singleNormal":
+        commands.push(
+          createCellCommand(
+            targetPosition,
+            {
+              type: "singleNormal",
+              colorId: getImageColorId(lower, targetPosition),
+              height: baseHeight + 1,
+              direction,
+            },
+          ),
+        );
+        setCell(targetPosition, baseHeight + 1);
+        break;
+      case "singleLight":
+        throw new Error("Unexpected singleLight");
+      case "dualFallen":
+        commands.push(
+          createCellCommand(
+            targetPosition,
+            {
+              type: "dualFallen",
+              lowerColorId: getImageColorId(lower, targetPosition),
+              upperColorId: getImageColorId(upper, targetPosition),
+              height: baseHeight + 1,
+              direction,
+            },
+          ),
+        );
+        setCell(targetPosition, undefined);
+        break;
+      case "dualNormal":
+        commands.push(
+          createCellCommand(
+            targetPosition,
+            {
+              type: "dualNormal",
+              lowerColorId: getImageColorId(lower, targetPosition),
+              upperColorId: getImageColorId(upper, targetPosition),
+              height: baseHeight + 1,
+              direction,
+            },
+          ),
+        );
+        setCell(targetPosition, baseHeight + 1);
+        break;
+      case "dualLight":
+        commands.push(
+          createCellCommand(
+            targetPosition,
+            {
+              type: "dualLight",
+              lowerColorId: getImageColorId(lower, targetPosition),
+              upperColorId: getImageColorId(upper, targetPosition),
+              height: baseHeight + 1,
+              direction,
+            },
+          ),
+        );
+        setCell(targetPosition, undefined);
+        break;
+    }
   }
 };
 
@@ -223,23 +418,35 @@ await Deno.writeFile(
   await img2025.encode(),
 );
 
-const imageMaskTemplate = createMaskTemplateImage(img2024, img2025);
+// const imageMaskTemplate = createMaskTemplateImage(img2024, img2025);
+
+// await Deno.writeFile(
+//   new URL(import.meta.resolve("./img/maskTemplate.png")),
+//   await imageMaskTemplate.encode(),
+// );
+
+const mask = discretizeImageColors(
+  await readImageFromFile(
+    new URL(import.meta.resolve("./img/mask.png")),
+  ),
+);
 
 await Deno.writeFile(
-  new URL(import.meta.resolve("./img/maskTemplate.png")),
-  await imageMaskTemplate.encode(),
+  new URL(import.meta.resolve("./img/mask-out.png")),
+  await mask.encode(),
 );
 
 const functionPath = (name: string): URL =>
   new URL(import.meta.resolve(`./art/data/art/function/${name}.mcfunction`));
 
-// await Deno.writeTextFile(
-//   functionPath("m"),
-//   createCommands(
-//     img2025,
-//     img2024,
-//   ),
-// );
+await Deno.writeTextFile(
+  functionPath("m"),
+  createCommands({
+    mask,
+    lower: img2025,
+    upper: img2024,
+  }),
+);
 
 await Deno.writeTextFile(
   functionPath("c"),
