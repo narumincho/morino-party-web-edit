6aef275bf4f8d02912c6055bfc8269038bc881f0
narumincho <16481886+narumincho@users.noreply.github.com>
2025-06-20 10:41:01 +0900

    add cross

--- deno/tower/main.ts
+++ deno/tower/main.ts
@@ -73,22 +73,44 @@ function lineBresenham3D(a: Vec3, b: Vec3): ReadonlyArray<Vec3> {
   return points;
 }
 
-const outsideA = { x: 41, y: -41, z: 9 };
-const outsideG = { x: 26, y: -26, z: 40 };
-const outsideH = { x: 21, y: -21, z: 54 };
-const outsideI = { x: 17, y: -17, z: 66 };
-const outsideJ = { x: 14, y: -14, z: 77 };
-const outsideK = { x: 13, y: -13, z: 87 };
-const outsideL = { x: 11, y: -11, z: 97 };
-const outsideM = { x: 10, y: -10, z: 105 };
-const outsideN = { x: 10, y: -10, z: 115 };
-const outsideO = { x: 9, y: -9, z: 120 };
-
-const toCenter = (position: Vec3): Vec3 => ({
+const setY = (position: Vec3, y: number) => ({
   x: position.x,
-  y: 0,
+  y,
   z: position.z,
 });
+
+const toCenter = (position: Vec3): Vec3 => setY(position, 0);
+
+const outerA: Vec3 = { x: 41, y: -41, z: 9 };
+const outerG: Vec3 = { x: 26, y: -26, z: 40 };
+const outerH: Vec3 = { x: 21, y: -21, z: 54 };
+const outerI: Vec3 = { x: 17, y: -17, z: 66 };
+const outerJ: Vec3 = { x: 14, y: -14, z: 77 };
+const outerK: Vec3 = { x: 13, y: -13, z: 87 };
+const outerL: Vec3 = { x: 11, y: -11, z: 97 };
+const outerM: Vec3 = { x: 10, y: -10, z: 105 };
+const outerN: Vec3 = { x: 10, y: -10, z: 115 };
+const outerO: Vec3 = { x: 9, y: -9, z: 120 };
+
+const innerG: Vec3 = setY(outerG, -12);
+const innerH: Vec3 = setY(outerH, -10);
+const innerI: Vec3 = setY(outerI, -7);
+const innerJ: Vec3 = setY(outerJ, -7);
+const innerK: Vec3 = setY(outerK, -6);
+const innerL: Vec3 = setY(outerL, -5);
+const innerM: Vec3 = setY(outerM, -5);
+const innerN: Vec3 = setY(outerN, -5);
+const innerO: Vec3 = setY(outerO, -4);
+
+const centerG = toCenter(outerG);
+const centerH = toCenter(outerH);
+const centerI = toCenter(outerI);
+const centerJ = toCenter(outerJ);
+const centerK = toCenter(outerK);
+const centerL = toCenter(outerL);
+const centerM = toCenter(outerM);
+const centerN = toCenter(outerN);
+const centerO = toCenter(outerO);
 
 const toSymmetry = (position: Vec3): ReadonlyArray<Vec3> => {
   return [
@@ -106,32 +128,61 @@ const toSymmetry = (position: Vec3): ReadonlyArray<Vec3> => {
 // position-check.blend グローバル
 const pointPairList: ReadonlyArray<readonly [Vec3, Vec3]> = [
   // ノ
-  [outsideA, outsideG],
-  [outsideG, outsideH],
-  [outsideH, outsideI],
-  [outsideI, outsideJ],
-  [outsideJ, outsideK],
-  [outsideK, outsideL],
-  [outsideL, outsideM],
-  [outsideM, outsideN],
-  [outsideN, outsideO],
+  [outerA, outerG],
+  [outerG, outerH],
+  [outerH, outerI],
+  [outerI, outerJ],
+  [outerJ, outerK],
+  [outerK, outerL],
+  [outerL, outerM],
+  [outerM, outerN],
+  [outerN, outerO],
   // 一
-  [outsideG, toCenter(outsideG)],
-  [outsideH, toCenter(outsideH)],
-  [outsideI, toCenter(outsideI)],
-  [outsideJ, toCenter(outsideJ)],
-  [outsideK, toCenter(outsideK)],
-  [outsideL, toCenter(outsideL)],
-  [outsideM, toCenter(outsideM)],
-  [outsideN, toCenter(outsideN)],
+  [outerG, centerG],
+  [outerH, centerH],
+  [outerI, centerI],
+  [outerJ, centerJ],
+  [outerK, centerK],
+  [outerL, centerL],
+  [outerM, centerM],
+  [outerN, centerN],
+  // inner ｜
+  [innerG, innerH],
+  [innerH, innerI],
+  [innerI, innerJ],
+  [innerJ, innerK],
+  [innerK, innerL],
+  [innerL, innerM],
+  [innerM, innerN],
+  [innerN, innerO],
   // ｜
-  [toCenter(outsideG), toCenter(outsideH)],
-  [toCenter(outsideH), toCenter(outsideI)],
-  [toCenter(outsideI), toCenter(outsideJ)],
-  [toCenter(outsideJ), toCenter(outsideK)],
-  [toCenter(outsideK), toCenter(outsideL)],
-  [toCenter(outsideL), toCenter(outsideM)],
-  [toCenter(outsideM), toCenter(outsideN)],
+  [centerG, centerH],
+  [centerH, centerI],
+  [centerI, centerJ],
+  [centerJ, centerK],
+  [centerK, centerL],
+  [centerL, centerM],
+  [centerM, centerN],
+  [centerN, centerO],
+  // X
+  [outerG, innerH],
+  [innerG, outerH],
+  [innerG, centerH],
+  [centerG, innerH],
+  [innerH, outerI],
+  [innerH, centerI],
+  [outerI, innerJ],
+  [centerI, innerJ],
+  [innerJ, outerK],
+  [innerJ, centerK],
+  [outerK, innerL],
+  [centerK, innerL],
+  [innerL, outerM],
+  [innerL, centerM],
+  [outerM, innerN],
+  [centerM, innerN],
+  [innerN, outerO],
+  [innerN, centerO],
 ];
 
 const positions: ReadonlyArray<Vec3> = pointPairList.flatMap(([a, b]) =>
